48599d32b133c5a63013bf0ad8a2525ddf8c3d1d
daveen99 <prospecs1039@daum.net>
2023-08-10 14:50:27 +0900

    동적 데이터 렌더링

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import React from 'react';
-import './App.css';
-import Appitem from './Appitem';
-import Noname from './Noname';
+import Expenseitem from './components/Expenseitem';
 
-const App() => {
-  //                    JSX문법
-  // const $h2 = React.createElement('h2', null, '반가워요');
-  // const $h2 = <h2>반가워요~</h2>;
+const App = () => {
   return (
     // 형제를 넣고싶을때는 React.Fragment로 감싸준다
     <>
-      <AppItem />
-      <Hello />
-      <Noname />
+      <Expenseitem />
+    </>
   );
-}
+};
 
 export default App;
