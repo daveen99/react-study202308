6e0e6580fe24f1daadc501cbe495adec2ddd3e8b
daveen99 <prospecs1039@daum.net>
2023-08-10 14:30:38 +0900

    컴포넌트 기초

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import './App.css';
 import React from 'react';
+import './App.css';
+import Appitem from './Appitem';
+import Noname from './Noname';
 
-function App() {
+const App() => {
   //                    JSX문법
   // const $h2 = React.createElement('h2', null, '반가워요');
-  const $h2 = <h2>반가워요~</h2>;
+  // const $h2 = <h2>반가워요~</h2>;
   return (
     // 형제를 넣고싶을때는 React.Fragment로 감싸준다
     <>
-      <div className="App">
-        <h1>메롱메롱</h1>
-        {$h2}
-      </div>
-
-      <div className="noname">
-        <input type="text" />
-        <p>
-          오늘은 태풍이 와서 비가 많이 내렸따.<br/>
-          그래서 공부가 하기 싫다.
-        </p>
-      </div>
-    </>
+      <AppItem />
+      <Hello />
+      <Noname />
   );
 }
 
---
+++ src/Appitem.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+
+class Appitem extends React.Component {
+
+    render() {
+        return (
+            <div className="App">
+            <h1>메롱메롱</h1>
+          </div>
+        );
+    };
+}; 
+
+export default Appitem;
