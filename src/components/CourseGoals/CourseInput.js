3cd46b1df6fe8d2dbe28bd2b67181fd3fa8ced38
daveen99 <prospecs1039@daum.net>
2023-08-17 11:26:38 +0900

    동적 인라인 스타일 변경

--- src/components/CourseGoals/CourseInput.js
+++ src/components/CourseGoals/CourseInput.js
@@ -4,8 +4,13 @@ import Button from '../UI/Button/Button';
 
 const CourseInput = ({onAdd}) => {
   const [enteredText, setEnteredText] = useState('');
+  // 입력값 검증 상태변수
+  const [isValid, setIsValid] = useState(true);
 
   const textChangeHandler = (e) => {
+    if (e.target.value.trim() > 0) {
+        setIsValid(true);
+    }
     // console.log(e.target.value);
     setEnteredText(e.target.value);
   };
@@ -14,6 +19,10 @@ const CourseInput = ({onAdd}) => {
     e.preventDefault();
     // console.log(enteredText);
 
+    if (enteredText.trim().length === 0) {
+        setIsValid(false);
+        return;
+    }
 
     onAdd(enteredText);
     setEnteredText('');
@@ -24,6 +33,10 @@ const CourseInput = ({onAdd}) => {
       <div className="form-control">
         <label>나의 목표</label>
         <input
+          style={{
+            background: isValid ? 'transparent': 'salmon',
+            borderColor: isValid ? 'black' : 'red',
+          }}
           type="text"
           onChange={textChangeHandler}
           value={enteredText}
