448d83fbf864aea3609a4af2ea4d6747ff4ebc67
daveen99 <prospecs1039@daum.net>
2023-08-18 09:24:08 +0900

    styled components 학습

--- src/components/CourseGoals/CourseList.js
+++ src/components/CourseGoals/CourseList.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import './CourseList.css';
+// import './CourseList.css';
 import CourceItem from './CourceItem';
+
+import styled from 'styled-components';
+
+const CourseUl = styled.ul`
+  list-style: none;
+  margin: 0;
+  padding: 0;
+`;
 
 const CourseList = ({ items, onDelete }) => {
   return (
-    <ul className="goal-list">
+    <CourseUl>
       {items.map((item) => {
         return (
           <CourceItem
@@ -14,7 +22,7 @@ const CourseList = ({ items, onDelete }) => {
           />
         );
       })}
-    </ul>
+    </CourseUl>
   );
 };
 
