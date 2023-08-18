4fae5fb721bf30a3e6b31f8d5aa1e36fb4b9f40a
daveen99 <prospecs1039@daum.net>
2023-08-18 10:50:03 +0900

    에러 모달 만들기

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AddUsers from './components/Users/AddUsers';
 import UserList from './components/Users/UserList';
 
 const App = () => {
+
+  const [userList, setUserList] = useState([]);
+
+  const addUserHandelr = user => {
+    console.log(user);
+    setUserList(prev => [...prev, {...user, id: Math.random().toString()}]);
+  };
+
   return (
-    <>
-      <AddUsers />
-      <UserList />
-    </>
+    <div>
+      <AddUsers onAddUser={addUserHandelr} />
+      <UserList users={userList} />
+    </div>
   );
 };
 
