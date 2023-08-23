2f8c22f84303510d2e520016bc40c62377bfb851
daveen99 <prospecs1039@daum.net>
2023-08-23 10:42:17 +0900

    Input 컴포넌트로 분할

--- src/components/SideEffect/Login/Login.js
+++ src/components/SideEffect/Login/Login.js
@@ -4,7 +4,7 @@ import Card from '../../UI/Card';
 import styles from './Login.module.css';
 import Button from '../../UI/Button/Button';
 import AuthContext from '../../../store/auth-context';
-
+import Input from '../../UI/Input/Input';
 
 // 리듀서 함수
 /*
@@ -25,20 +25,19 @@ const emailReducer = (state, action) => {
   if (action.type === 'USER_INPUT') {
     return {
       value: action.val,
-      isValid: action.val.includes('@')
+      isValid: action.val.includes('@'),
     };
   } else if (action.type === 'INPUT_VALIDATE') {
     return {
       value: state.value,
-      isValid: state.value.includes('@')
+      isValid: state.value.includes('@'),
     };
   }
   return {
     value: '',
-    isVaild: null
+    isVaild: null,
   };
 };
-
 
 const passwordReducer = (state, action) => {
   if (action.type === 'USER_INPUT') {
@@ -54,13 +53,11 @@ const passwordReducer = (state, action) => {
   }
   return {
     value: '',
-    isVaild: null
+    isVaild: null,
   };
 };
 
-
 const Login = () => {
-  
   const { onLogin } = useContext(AuthContext);
 
   // email reducer 사용하기
@@ -72,17 +69,16 @@ const Login = () => {
   */
   const [emailState, dispatchEmail] = useReducer(emailReducer, {
     value: '',
-    isVaild: null
+    isVaild: null,
   });
 
   const [passwordState, dispatchPassword] = useReducer(passwordReducer, {
     value: '',
-    isVaild: null
+    isVaild: null,
   });
 
   // 이메일, 패스워드가 둘다 동시에 정상적인 상태인지 확인
   const [formIsValid, setFormIsValid] = useState(false);
-
 
   // emailState의 isValid가 변경됐을 때만 useEffect를 실행하게 하려면
   // isValid를 디스트럭쳐링 한다
@@ -92,12 +88,9 @@ const Login = () => {
   // 입력란을 모두 체크하여 form의 버튼 disabled를 해제하는 상태변수
   // formIsValid의 사이드 이펙트를 처리하는 영역
   useEffect(() => {
-
     const timer = setTimeout(() => {
       console.log('useEffect call in Login.js');
-      setFormIsValid(
-        emailIsValid && passwordIsValid
-      );
+      setFormIsValid(emailIsValid && passwordIsValid);
     }, 1000);
 
     // cleanup 함수 - 컴포넌트가 업데이트되거나 없어지기 전에 실행
@@ -105,7 +98,6 @@ const Login = () => {
       console.log('clean up!!');
       clearTimeout(timer);
     };
-
   }, [emailIsValid, passwordIsValid]); // 이 배열에 상태변수를 넣어주면 그 상태변수가 바뀔때마다 useEffect를 재실행함
 
   const emailChangeHandler = (e) => {
@@ -113,28 +105,28 @@ const Login = () => {
     /*
       param1 - action객체(리듀서 함수의 2번째 파라미터)
     */
-    dispatchEmail({ 
+    dispatchEmail({
       type: 'USER_INPUT',
-      val: e.target.value
+      val: e.target.value,
     });
   };
 
   const passwordChangeHandler = (e) => {
-    dispatchPassword({ 
+    dispatchPassword({
       type: 'USER_INPUT',
-      val: e.target.value
+      val: e.target.value,
     });
   };
 
   const validateEmailHandler = () => {
-    dispatchEmail({ 
-      type: 'INPUT_VALIDATE'
+    dispatchEmail({
+      type: 'INPUT_VALIDATE',
     });
   };
 
   const validatePasswordHandler = () => {
-    dispatchPassword({ 
-      type: 'INPUT_VALIDATE'
+    dispatchPassword({
+      type: 'INPUT_VALIDATE',
     });
   };
 
@@ -146,34 +138,25 @@ const Login = () => {
   return (
     <Card className={styles.login}>
       <form onSubmit={submitHandler}>
-        <div
-          className={`${styles.control} ${
-            emailIsValid === false ? styles.invalid : ''
-          }`}
-        >
-          <label htmlFor="email">E-Mail</label>
-          <input
-            type="email"
-            id="email"
-            value={emailState.value}
-            onChange={emailChangeHandler}
-            onBlur={validateEmailHandler}
-          />
-        </div>
-        <div
-          className={`${styles.control} ${
-            passwordIsValid === false ? styles.invalid : ''
-          }`}
-        >
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            id="password"
-            value={passwordState.value}
-            onChange={passwordChangeHandler}
-            onBlur={validatePasswordHandler}
-          />
-        </div>
+        <Input
+          id="email"
+          type="email"
+          label="E-Mail"
+          value={emailState.value}
+          IsValid={emailIsValid}
+          onChange={emailChangeHandler}
+          onBlur={validateEmailHandler}
+        />
+        <Input
+          id="password"
+          type="password"
+          label="Password"
+          value={passwordState.value}
+          IsValid={passwordIsValid}
+          onChange={passwordChangeHandler}
+          onBlur={validatePasswordHandler}
+        />
+
         <div className={styles.actions}>
           <Button
             type="submit"
