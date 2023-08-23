635fc35645d75a52f8b60df59ad069e6010565ac
daveen99 <prospecs1039@daum.net>
2023-08-23 10:27:48 +0900

    useContext와 리팩토링

--- src/App.js
+++ src/App.js
@@ -1,53 +1,24 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import MainHeader from './components/SideEffect/MainHeader/MainHeader';
 import Home from './components/SideEffect/Home/Home';
 import Login from './components/SideEffect/Login/Login';
-//컨텍스트 불러오기
 import AuthContext from './store/auth-context';
 
+
 const App = () => {
-  // 로그인 상태를 관리하는 변수
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  // 화면이 리렌더링될 때 localStorage를 확인해서
-  // 현재 로그인 플래그가 존재하는지 검사
+  const { isLoggedIn } = useContext(AuthContext);
 
-  // 기존에 로그인한 사람인지 확인하는 코드는 리렌더링할때마다
-  // 작동하게 만들면 안된다.
-  useEffect(() => {
-    console.log('useEffect 실행! - 최초 단한번만 실행됨!');
-    console.log(`로그인 검사 수행!`);
 
-    const storedLoginFlag = localStorage.getItem('login-flag');
-    if (storedLoginFlag === '1') {
-      setIsLoggedIn(true);
-    }
-  }, []);
-
-  // 서버로 로그인을 요청하는 함수
-  const loginHandler = ({ email, password }) => {
-    // 로그인의 증거로 브라우저에 로그인 값을 1로 표현해서 저장
-    localStorage.setItem('login-flag', '1');
-    setIsLoggedIn(true);
-    console.log('로그인!');
-  };
-
-  const logoutHandler = () => {
-    localStorage.removeItem('login-flag');
-    setIsLoggedIn(false);
-    console.log('로그아웃!');
-  };
 
   return (
-    <AuthContext.Provider value={{
-      isLoggedIn: isLoggedIn
-    }}>
-      <MainHeader onLogout={logoutHandler} />
+    <>
+      <MainHeader />
       <main>
         {isLoggedIn && <Home />}
-        {!isLoggedIn && <Login onLogin={loginHandler} />}
+        {!isLoggedIn && <Login />}
       </main>
-    </AuthContext.Provider>
+    </>
   );
 };
 
--- src/components/SideEffect/Navigation/Navigation.js
+++ src/components/SideEffect/Navigation/Navigation.js
@@ -1,35 +1,31 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import classes from './Navigation.module.css';
 import AuthContext from '../../../store/auth-context';
 
-const Navigation = ({ onLogout }) => {
+const Navigation = () => {
+  const authCtx = useContext(AuthContext);
+
   return (
-    <AuthContext.Consumer>
-      {(context) => {
-        return (
-          <nav className={classes.nav}>
-            <ul>
-              {context.isLoggedIn && (
-                <li>
-                  <a href="/">Users</a>
-                </li>
-              )}
-              {context.isLoggedIn && (
-                <li>
-                  <a href="/">Admin</a>
-                </li>
-              )}
-              {context.isLoggedIn && (
-                <li>
-                  <button onClick={onLogout}>Logout</button>
-                </li>
-              )}
-            </ul>
-          </nav>
-        );
-      }}
-    </AuthContext.Consumer>
+    <nav className={classes.nav}>
+      <ul>
+        {authCtx.isLoggedIn && (
+          <li>
+            <a href="/">Users</a>
+          </li>
+        )}
+        {authCtx.isLoggedIn && (
+          <li>
+            <a href="/">Admin</a>
+          </li>
+        )}
+        {authCtx.isLoggedIn && (
+          <li>
+            <button onClick={authCtx.onLogout}>Logout</button>
+          </li>
+        )}
+      </ul>
+    </nav>
   );
 };
 
